33c296bfcd665a03f203f46f8c68f3fef9e6a0d1
Mustafa Oğuzcan Kaçmaz <oguzcankacmaz@hotmail.com>
2023-02-02 14:20:21 +0300

    Add project files.

---
+++ NotDefteriApi/Controllers/NotlarController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotDefteriApi.Data;
+using NotDefteriApi.Dtos;
+
+namespace NotDefteriApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotlarController : ControllerBase
+    {
+        private readonly UygulamaDbContext _db;
+
+        public NotlarController(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<List<Not>> GetNotlar()
+        {
+            return await _db.Notlar.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Not>> GetNot(int id)
+        {
+            Not? not = await _db.Notlar.FindAsync(id);
+            if (not == null)
+                return NotFound();
+
+            return not;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Not>> PostNot(YeniNotDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                Not not = new()
+                {
+                    Baslik = dto.Baslik,
+                    Icerik = dto.Icerik
+                };
+                _db.Notlar.Add(not);
+                await _db.SaveChangesAsync();
+                return not;
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Not>> PutNot(GuncelleNotDto dto, int id)
+        {
+            if (id != dto.Id)
+                return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                var not = await _db.Notlar.FindAsync(id);
+                if (not == null)
+                    return NotFound();
+                not.Icerik = dto.Icerik;
+                not.Baslik = dto.Baslik;
+                not.DegistirmeZamani = DateTime.Now;
+
+                await _db.SaveChangesAsync();
+                return not;
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNot(int id)
+        {
+            var not = await _db.Notlar.FindAsync(id);
+            if (not == null)
+                return NotFound();
+            _db.Notlar.Remove(not);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ NotDefteriApi/Dtos/YeniNotDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NotDefteriApi.Dtos
+{
+    public class YeniNotDto
+    {
+        public string Baslik { get; set; } = null!;
+        public string? Icerik { get; set; } = string.Empty;
+    }
+}
